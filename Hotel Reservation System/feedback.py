7594fef4614a96dc5246c4a499697ba164d038ca
Adli Arif <adliarif9461@gmail.com>
2024-06-09 21:29:31 +0800

    update class for feedbackpage

---
+++ Hotel Reservation System/feedback.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+
+class FeedbackPage:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("K I N G S T O N  H O T E L - FEEDBACK")
+        self.root.geometry("600x400")
+        self.root.configure(bg="#c9c1a7")
+
+        
