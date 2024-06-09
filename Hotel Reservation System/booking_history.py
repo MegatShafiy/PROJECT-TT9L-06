23fe561cbc712ab76da39e2e55d8c4dd2643e9ab
Adli Arif <adliarif9461@gmail.com>
2024-06-09 22:03:40 +0800

    create mainframe

--- Hotel Reservation System/booking_history.py
+++ Hotel Reservation System/booking_history.py
@@ -1,4 +1,20 @@
 from tkinter import *
 from tkinter import ttk
 
+class BookingHistoryPage:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("K I N G S T O N  H O T E L - BOOKING HISTORY")
+        self.root.geometry("800x600")
+        self.root.configure(bg="#c9c1a7")
 
+        # Create main frame
+        self.main_frame = Frame(self.root, bg="#c9c1a7")
+        self.main_frame.pack(fill=BOTH, expand=True, padx=20, pady=20)
+
+        
+
+        
+        
+        
+        
