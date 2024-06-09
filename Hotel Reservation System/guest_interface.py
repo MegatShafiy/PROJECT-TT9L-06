f43059abddbb22919cab15858c9cacc46ffb91ea
Adli Arif <adliarif9461@gmail.com>
2024-06-09 21:21:51 +0800

    create class for guestinterface

--- Hotel Reservation System/guest_interface.py
+++ Hotel Reservation System/guest_interface.py
@@ -4,4 +4,11 @@ import feedback
 import booking_history
 import room_availability
 
+class GuestInterface:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("K I N G S T O N  H O T E L - GUEST INTERFACE")
+        self.root.geometry("800x600")
+        self.root.configure(bg="#c9c1a7")
 
+        
