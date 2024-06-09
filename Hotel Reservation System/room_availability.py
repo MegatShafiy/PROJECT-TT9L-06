751024fade902bef25679bddc1ff02bfdcb8aef2
Adli Arif <adliarif9461@gmail.com>
2024-06-09 21:43:30 +0800

    create mainframe

--- Hotel Reservation System/room_availability.py
+++ Hotel Reservation System/room_availability.py
@@ -1,4 +1,12 @@
 from tkinter import *
 from tkinter import ttk
 
+class RoomAvailabilityPage:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("K I N G S T O N  H O T E L - ROOM AVAILABILITY")
+        self.root.geometry("600x400")
+        self.root.configure(bg="#c9c1a7")
+
+
 
